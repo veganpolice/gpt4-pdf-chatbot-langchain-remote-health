65d819f39944e40e4aee5d0d356f8f566053626a
veganpolice <>
2023-06-25 01:47:58 -0700

    back to esnext

--- pages/index.tsx
+++ pages/index.tsx
@@ -137,7 +137,7 @@ export default function Home() {
                     icon = (
                       <Image
                         key={index}
-                        src="/bot-image.png"
+                        src="/bird-image.png"
                         alt="AI"
                         width="40"
                         height="40"
